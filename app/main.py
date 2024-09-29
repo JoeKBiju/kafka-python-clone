0d2213361494543c61efeea1e45d1e38ec609a39
joseph <joebiju8@gmail.com>
2024-09-29 18:01:05 +0100

    code change

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def create_message(id: int, api_key: int, api_version: int):
     #Body
     if (api_version == 35):
         response_bytes += api_version.to_bytes(2, 'big')
-    response_bytes += supported_api_keys.to_bytes(1, 'big')
+    response_bytes += supported_api_keys.to_bytes(2, 'big')
     response_bytes += api_key.to_bytes(2, 'big')
     response_bytes += min_version.to_bytes(2, 'big')
     response_bytes += max_version.to_bytes(2, 'big')

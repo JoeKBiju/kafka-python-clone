25de097ce35129bc216f9245143ba7476cde1ad7
joseph <joebiju8@gmail.com>
2024-09-29 16:41:01 +0100

    added parse_correlation_id function

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import socket
 
-def create_message():
-    id = 7
+def create_message(id):
     id_bytes = id.to_bytes(4, 'big')
-    return len(id_bytes).to_bytes(4, 'big') + id_bytes
+    return_message = len(id_bytes).to_bytes(4, 'big') + id_bytes
+    return return_message
 
+def get_request_length(request):
+    length_bytes = request[0:4]
+    length = int.from_bytes(length_bytes, 'big')
+    return length
+
+def parse_correlation(request):
+    correlation_id = int.to_bytes(request[8:12], 'big')
+    return correlation_id
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
-    print(request)
-    client_socket.sendall(create_message())
+    correlation_id = parse_correlation(request)
+    client_socket.sendall(create_message(correlation_id))
     client_socket.close()
 
 def main():

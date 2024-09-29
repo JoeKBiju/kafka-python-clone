7f082d22a3d0be534ac232487e34b7c08909c1f0
joseph <joebiju8@gmail.com>
2024-09-29 16:23:21 +0100

    set debugging to true in yaml

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-import socket  # noqa: F401
+import socket
 
 def create_message():
     id = 7
     id_bytes = id.to_bytes(4, 'big')
     return len(id_bytes).to_bytes(4, 'big') + id_bytes
 
+
 def handle_client(client_socket):
     request = client_socket.recv(1024)
+    print(request)
     client_socket.sendall(create_message())
     client_socket.close()
 

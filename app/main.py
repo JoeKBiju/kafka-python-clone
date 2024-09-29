475e420b3cdd3dfab45712a040087f8e4d7f5469
joseph <joebiju8@gmail.com>
2024-09-29 14:40:53 +0100

    can send response header now

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import socket  # noqa: F401
 
+def create_message():
+    return "\x0000000800000007"
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    client_socket.send(create_message())
+    client_socket.close()
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept()
-    server.send(hex(7))
+    client_socket, client_addr = server.accept()
 
 if __name__ == "__main__":
     main()

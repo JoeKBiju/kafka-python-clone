2fbf13953f107c0029df2a7718860eadbb7ad98b
joseph <joebiju8@gmail.com>
2024-09-29 14:45:14 +0100

    fix: called handle client fucntion from main

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ def create_message():
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
-    client_socket.sendall(create_message())
+    client_socket.send(create_message())
     client_socket.close()
 
 def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     client_socket, client_addr = server.accept()
+    handle_client(client_socket)
 
 if __name__ == "__main__":
     main()
